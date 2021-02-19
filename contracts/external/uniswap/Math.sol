1575e5182da52af9531f4081a82e1e1ac539932a
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2021-02-19 21:26:56 +0300

    feat: added Uniswap pair interface + sqrt math lib

---
+++ contracts/external/uniswap/Math.sol
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+pragma solidity ^0.7.0;
+
+// https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/libraries/Math.sol
+
+// a library for performing various math operations
+
+library Math {
+    function min(uint x, uint y) internal pure returns (uint z) {
+        z = x < y ? x : y;
+    }
+
+    // babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+}
